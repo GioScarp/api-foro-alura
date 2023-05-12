872ebc305bcf9015bb95ff3d3f23708f0d8e1223
Giovanni Scarpetta <gioovanniscarpetta@gmail.com>
2023-05-12 15:25:24 -0500

    Iniciando proyecto foro alura
    Iniciando proyecto foro alura

---
+++ src/test/java/gio/apiforoalura/ApiForoAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package gio.apiforoalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForoAluraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
